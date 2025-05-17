a961a5f8a8ac2ad79275e8744f4c8b02ee41ba1b
Rain <rainkalugdan@gmail.com>
2025-05-17 17:31:00 +0800

    feat(error handling): render UI with error message

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,33 @@ document
   .querySelector("#film-form")
   .addEventListener("submit", handleFilmSearch);
 
-// Fetch film data from OMDB API
+function renderError(message) {
+  document.querySelector(".film-results-container").innerHTML = `
+  <p class="state-message">${message}</p>`;
+}
+
+// Fetch film data from OMDB API based on user input
 async function fetchFilmResults(filmInput) {
   try {
+    // Make a request to the OMDB API with the search query
     const response = await fetch(
       `http://www.omdbapi.com/?apikey=efde29a&s=${filmInput}`
     );
     if (!response.ok) throw new Error("Network response was not ok");
     const data = await response.json();
-    console.log(data);
-    renderFilmResults();
+
+    // If the API returns no results, show an error message and exit
+    if (data.Response === "False") {
+      const message =
+        "Unable to find what you’re looking for. Please try another search.";
+      renderError(message);
+      return;
+    }
+    // Render the list of films if results are found
+    renderFilmResults(data.Search);
   } catch (error) {
-    const message =
-      "Unable to find what you’re looking for. Please try another search.";
+    console.error(error);
+    const message = "Error fetching data.";
     renderError(message);
   }
 }
